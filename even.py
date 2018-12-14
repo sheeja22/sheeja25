75929de2b36da4b04186a921bb692e27024231e4
sheeja22 <45845291+sheeja22@users.noreply.github.com>
2018-12-14 15:37:23 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,8 +1,8 @@
-# python program to check if the input number is odd or even.
-# A number is even if division by 2 give a remainder of 0.
+# Python program to check if the input number is odd or even.
+# a number is even if division by 2 give a remainder of 0.
 # if remainder is 1, it is odd number.
 Num = int(input("Enter a number: "))
-if (num % 2) == 0:
+If (num % 2) == 0:
    Print("{0} is Even".format(num))
-else:
+Else:
    Print("{0} is Odd".format(num))
